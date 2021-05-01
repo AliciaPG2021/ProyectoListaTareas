dda45ee20db989a27fdc162da5b8fd0ca2b4866e
Alicia <aliciapradag@gmail.com>
2021-05-01 18:47:00 +0200

    Creación Array de tareas

--- js/main.js
+++ js/main.js
@@ -1,2 +1,34 @@
+const tareasNuevas = new Array();
+
+let inputText = document.querySelector('.tarea');
+let boton = document.querySelector('.boton');
+let selectPrioridad = document.querySelector('.seleccionTarea');
+
+boton.addEventListener('click', recogerTareas);
+
+function recogerTareas(event) {
+
+    event.preventDefault();
+
+    let textoTarea = inputText.value.trim();
+    let seleccionTarea = selectPrioridad.value;
+
+
+    if (textoTarea !== "" && seleccionTarea !== 0) {
+
+        const nuevaTarea = {
+            texto: textoTarea,
+            prioridad: seleccionTarea,
+        }
+        tareasNuevas.push(nuevaTarea);
+
+    } else {
+        alert('Todos los campos son obligatorios');
+    }
+
+
+    console.log(tareasNuevas);
+}
+
 
 
